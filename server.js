466197142df648e48d48588b1d10ab9ccfac698b
devihall <devis0306@gmail.com>
2022-08-20 14:55:43 -0400

    environment variable port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const { animals } = require("./data/animals");
 
 const express = require("express");
 
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 app.get('/api/animals', (req, res) => {
@@ -12,8 +14,8 @@ app.get('/api/animals', (req, res) => {
   res.json(results);
 });
 
-app.listen(3001, () => {
-  console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
 });
 
 function filterByQuery(query, animalsArray) {
