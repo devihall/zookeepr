080289267fbcfe3c177de42f93d9b3256a241b08
devihall <devis0306@gmail.com>
2022-08-22 14:34:57 -0400

    implement front end functionality

--- server.js
+++ server.js
@@ -8,10 +8,13 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// parse incoming string or array data
+// middleware-parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+//middleware to serve static files
+app.use(express.static("public"));
+
 
 function filterByQuery(query, animalsArray) {
   let personalityTraitsArray = [];
@@ -96,7 +99,6 @@ function validateAnimal(animal) {
 }
 
 
-
 app.get("/api/animals", (req, res) => {
   let results = animals;
   if (req.query) {
@@ -131,6 +133,20 @@ app.post("/api/animals", (req, res) => {
   
   res.json(req.body);
   }
+});
+
+////routes to serve files////
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+app.get("/animals", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/animals.html"));
+});
+app.get("/zookeepers", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/zookeepers.html"));
+});
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 
