5d0bd5938fb64d43cdbc2af83c5d89c928db8fa4
devihall <devis0306@gmail.com>
2022-08-25 16:22:49 -0400

    removed non functional route

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -11,8 +11,8 @@ router.get("/animals", (req, res) => {
 router.get("/zookeepers", (req, res) => {
   res.sendFile(path.join(__dirname, "../../public/zookeepers.html"));
 });
-router.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../../public/index.html"));
-});
+// router.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../../public/index.html"));
+// });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const fs = require("fs");
-const path = require("path");
-
 const express = require("express");
+const fs = require("fs");
+
+const path = require("path");
 
 const apiRoutes = require("./routes/apiRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
